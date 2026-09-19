b52b212a1fa56b9d0682f53f607cf26a3223fa5c
vaishnavi13222 <vaishnavi.13k@gmail.com>
2026-09-19 12:24:51 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,28 @@
+print("hello class")
+age=int(input("enter a age"))
+if(age>=18):
+    print("eligible for vote")
+    print("eligible for bike ride")
+else:
+    print("not eligible for vote")
+    print("not eligible for bike ride")
+print("Enter your name")
+#EXAMPLE FOR ELIF STATEMENT 
+number=int(input("enter a number:"))
+if(number==1):
+    print("MRDU")
+elif(number==2):
+    print("MBU")
+elif(number==3):
+    print(GITAM)
+else:
+    print("select another collage")
+
+number=int(input("enter a number:"))
+if(number>0):  
+    if(number<50):  
+        print("number in between 1-50")
+    else:
+        print("number is greater than 50")
+else:
+    print("-ve number")  
